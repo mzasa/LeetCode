30e7be6bbe46d15a7006e7c3f98e3feb0142ba49
mzasa <tonam870@gmail.com>
2024-01-16 23:12:48 +0700

    16-1-2024

--- 118. Pascal's Triangle 84.67% 70.82%/main.js
+++ 118. Pascal's Triangle 84.67% 70.82%/main.js
@@ -15,19 +15,3 @@ var generate = function(numRows) {
     return ans
 };
 
-var getRow = function(rowIndex) {
-    if(rowIndex == 1)
-        return [[1]]
-    if(rowIndex == 2)
-        return [[1],[1,1]]
-
-    let ans = [1,1]
-    for (let i = 0; i < rowIndex - 2; i++) {
-        const temp = [1]
-        for (let j = 0; j < i + 1; j++)
-            temp.push(ans[j] + ans[j + 1])
-        temp.push(1)
-        ans = temp.slice()
-    }
-    return ans
-};
--- 119. Pascal's Triangle II 75.63% 51.53%/main.js
+++ 119. Pascal's Triangle II 75.63% 51.53%/main.js
@@ -1,16 +1,16 @@
 var getRow = function(rowIndex) {
-    if(rowIndex == 0)
-        return [1]
     if(rowIndex == 1)
-        return [1,1]
+        return [[1]]
+    if(rowIndex == 2)
+        return [[1],[1,1]]
 
     let ans = [1,1]
-    for (let i = 0; i < rowIndex - 1; i++) {
+    for (let i = 0; i < rowIndex - 2; i++) {
         const temp = [1]
         for (let j = 0; j < i + 1; j++)
             temp.push(ans[j] + ans[j + 1])
         temp.push(1)
-        ans = temp
+        ans = temp.slice()
     }
     return ans
 };
