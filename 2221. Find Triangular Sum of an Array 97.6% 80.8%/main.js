75e51aefe75b2e32d21ce6855576a916a074caa1
mzasa <tonam870@gmail.com>
2024-01-16 23:26:39 +0700

    16-1-2024

--- 2221. Find Triangular Sum of an Array 97.6% 80.8%/main.js
+++ 2221. Find Triangular Sum of an Array 97.6% 80.8%/main.js
@@ -2,8 +2,6 @@ var triangularSum = function(nums) {
     for (let i = 0; i < nums.length - 1; ++i){
         for (let j = 0; j < nums.length - i - 1; ++j)
             nums[j] = (nums[j] + nums[j + 1]) % 10
-        for (let j = 0; j < nums.length; ++j)
-            console.log(nums[j])
     }
     return nums[0]
 };
