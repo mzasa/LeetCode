282249d3cba69fd7f81656ff2724b8c5028ff1c2
mzasa <tonam870@gmail.com>
2024-01-16 20:31:50 +0700

    16-1-2024

---
+++ 118. Pascal's Triangle 84.67% 70.82%/main.js
@@ -0,0 +1,16 @@
+var generate = function(numRows) {
+    if(numRows == 1)
+        return [[1]]
+    if(numRows == 2)
+        return [[1],[1,1]]
+
+    const ans = [[1],[1,1]]
+    for (let i = 0; i < numRows - 2; i++) {
+        const temp = [1]
+        for (let j = 0; j < i + 1; j++)
+            temp.push(ans[i + 1][j] + ans[i + 1][j + 1])
+        temp.push(1)
+        ans.push(temp)
+    }
+    return ans
+};
